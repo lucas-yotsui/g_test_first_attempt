7deba14e02f4043f252f379704a2a0f96f89a762
Lucas Yukio <yotsui@lisha.ufsc.br>
2023-08-21 09:41:14 -0300

    first commit

---
+++ cmake/CMakeOptions.cmake
@@ -0,0 +1,49 @@
+set(COMPILER_WARNINGS 
+        -Wall 
+        -Wextra 
+        -pedantic 
+        -MD 
+        -fno-exceptions 
+    )
+
+set(INCLUDE_DIR ${CMAKE_SOURCE_DIR}/include)
+
+# Use any extension you want bro
+file(GLOB_RECURSE SOURCES 
+        ${CMAKE_SOURCE_DIR}/source/*.c
+        ${CMAKE_SOURCE_DIR}/source/*.C # funny enough, this one is not a c equivalent, but rather a c++ one
+        ${CMAKE_SOURCE_DIR}/source/*.cpp
+        ${CMAKE_SOURCE_DIR}/source/*.CPP
+        ${CMAKE_SOURCE_DIR}/source/*.c++
+        ${CMAKE_SOURCE_DIR}/source/*.cxx
+        ${CMAKE_SOURCE_DIR}/source/*.cc
+        ${CMAKE_SOURCE_DIR}/source/*.cp
+        ${CMAKE_SOURCE_DIR}/app/*.c
+        ${CMAKE_SOURCE_DIR}/app/*.C # funny enough, this one is not a c equivalent, but rather a c++ one
+        ${CMAKE_SOURCE_DIR}/app/*.cpp
+        ${CMAKE_SOURCE_DIR}/app/*.CPP
+        ${CMAKE_SOURCE_DIR}/app/*.c++
+        ${CMAKE_SOURCE_DIR}/app/*.cxx
+        ${CMAKE_SOURCE_DIR}/app/*.cc
+        ${CMAKE_SOURCE_DIR}/app/*.cp
+    )
+    
+file(GLOB_RECURSE HEADERS 
+        ${INCLUDE_DIR}/*.h 
+        ${INCLUDE_DIR}/*.H # funny enough, this one is not a h equivalent, but rather a hpp one
+        ${INCLUDE_DIR}/*.hh 
+        ${INCLUDE_DIR}/*.hpp
+        ${INCLUDE_DIR}/*.h++
+        ${INCLUDE_DIR}/*.hxx
+    )
+    
+file(GLOB_RECURSE TESTS 
+        ${CMAKE_SOURCE_DIR}/test/*.c
+        ${CMAKE_SOURCE_DIR}/test/*.C # funny enough, this one is not a c equivalent, but rather a c++ one
+        ${CMAKE_SOURCE_DIR}/test/*.cpp
+        ${CMAKE_SOURCE_DIR}/test/*.CPP
+        ${CMAKE_SOURCE_DIR}/test/*.c++
+        ${CMAKE_SOURCE_DIR}/test/*.cxx
+        ${CMAKE_SOURCE_DIR}/test/*.cc
+        ${CMAKE_SOURCE_DIR}/test/*.cp
+    )
