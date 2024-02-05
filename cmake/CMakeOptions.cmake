ecf699ff96aa5ebfb2501dfb0cf7b067286aaa66
lucas-yotsui <lucasyotsui@hotmail.com>
2024-02-04 23:11:46 -0300

    This wouldn't work before, test executable was being compiled with the main.cpp file (there would have been two mains, kinda dumb mistake btw)

--- cmake/CMakeOptions.cmake
+++ cmake/CMakeOptions.cmake
@@ -10,40 +10,15 @@ set(INCLUDE_DIR ${CMAKE_SOURCE_DIR}/include)
 
 # Use any extension you want bro
 file(GLOB_RECURSE SOURCES 
-        ${CMAKE_SOURCE_DIR}/source/*.c
-        ${CMAKE_SOURCE_DIR}/source/*.C # funny enough, this one is not a c equivalent, but rather a c++ one
-        ${CMAKE_SOURCE_DIR}/source/*.cpp
-        ${CMAKE_SOURCE_DIR}/source/*.CPP
-        ${CMAKE_SOURCE_DIR}/source/*.c++
-        ${CMAKE_SOURCE_DIR}/source/*.cxx
-        ${CMAKE_SOURCE_DIR}/source/*.cc
-        ${CMAKE_SOURCE_DIR}/source/*.cp
-        ${CMAKE_SOURCE_DIR}/app/*.c
-        ${CMAKE_SOURCE_DIR}/app/*.C # funny enough, this one is not a c equivalent, but rather a c++ one
-        ${CMAKE_SOURCE_DIR}/app/*.cpp
-        ${CMAKE_SOURCE_DIR}/app/*.CPP
-        ${CMAKE_SOURCE_DIR}/app/*.c++
-        ${CMAKE_SOURCE_DIR}/app/*.cxx
-        ${CMAKE_SOURCE_DIR}/app/*.cc
-        ${CMAKE_SOURCE_DIR}/app/*.cp
-    )
+    ${CMAKE_SOURCE_DIR}/source/*.c
+    ${CMAKE_SOURCE_DIR}/source/*.cpp
+)
     
 file(GLOB_RECURSE HEADERS 
-        ${INCLUDE_DIR}/*.h 
-        ${INCLUDE_DIR}/*.H # funny enough, this one is not a h equivalent, but rather a hpp one
-        ${INCLUDE_DIR}/*.hh 
-        ${INCLUDE_DIR}/*.hpp
-        ${INCLUDE_DIR}/*.h++
-        ${INCLUDE_DIR}/*.hxx
-    )
+    ${INCLUDE_DIR}/*.h 
+    ${INCLUDE_DIR}/*.hpp
+)
     
 file(GLOB_RECURSE TESTS 
-        ${CMAKE_SOURCE_DIR}/tests/*.c
-        ${CMAKE_SOURCE_DIR}/tests/*.C # funny enough, this one is not a c equivalent, but rather a c++ one
-        ${CMAKE_SOURCE_DIR}/tests/*.cpp
-        ${CMAKE_SOURCE_DIR}/tests/*.CPP
-        ${CMAKE_SOURCE_DIR}/tests/*.c++
-        ${CMAKE_SOURCE_DIR}/tests/*.cxx
-        ${CMAKE_SOURCE_DIR}/tests/*.cc
-        ${CMAKE_SOURCE_DIR}/tests/*.cp
-    )
+    ${CMAKE_SOURCE_DIR}/tests/*.cpp
+)
